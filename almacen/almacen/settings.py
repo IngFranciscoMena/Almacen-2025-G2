faf57238d18201ca57de549509bb21a81ecd72f7
IngFranciscoMena <franciscodaniel.penate@unab.edu.sv>
2025-11-07 18:52:01 -0600

    se agregan variables de entorno para el debug y la secret key del proyecto

--- almacen/almacen/settings.py
+++ almacen/almacen/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0@45!c1xi&u%0gpdq_uviek0a0*y_1k7)i%rd90xha)=2rdfu1'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
